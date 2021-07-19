4dc138eb7ba16eb0bb9a3c31b5f2049271e050f4
RiccardoSalvalajo <salvalajo.riccardo.cartiera@gmail.com>
2021-07-19 12:01:52 +0200

    fine header

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -5,14 +5,14 @@
                     <span v-if="link.new">{{link.new}}</span>
                     <h5><a href="#">{{link.text}}</a></h5>
                     <ul class="dropdown" v-if="link.drops" >
-                        <li v-for="(downlink, index) in link.drops" :key="'A'+ index">
+                        <li :class="(downlink.styles) ? 'start-right' : ''" v-for="(downlink, index) in link.drops" :key="'A'+ index">
                             <a href="#">
                                 {{downlink.link}}
                                 <i v-if="downlink.styles" class="fas fa-chevron-right"></i> 
                             </a>
                             <ul v-if="downlink.styles">
                                 <li v-for="(last,index) in downlink.styles " :key="'B'+ index">
-                                    {{last.ext}}
+                                    <a href="#">{{last.ext}}</a>
                                 </li>
                             </ul>
                         </li>
@@ -49,7 +49,8 @@ export default {
                 background-color: #dc1245;
                 color: white;
                 font-size: 10px;
-                border-radius: 2px;
+                border-radius: 3px 3px 3px 0;
+                padding: 0 3px;
             }
         }
         .start:hover .dropdown{
@@ -64,15 +65,46 @@ export default {
             line-height: 40px;
             background-color: #fff;
             border-top: 3px solid #f3bf33;
-            & li:hover a{
+            &::after {
+                content: '';
+                position: absolute;
+                top:-10px;
+                left: 50%;
+                margin-left: -50px;
+                width: 0;
+                height: 0;
+                border-bottom: solid 10px #f3bf33;
+                border-left: solid 10px transparent;
+                border-right: solid 10px transparent;
+                }
+            & li:hover > a{
                 color:#f2b71d ;
             }
-            
+            i {
+                margin-left: 40px ;
+            }
+            .start-right{
+                position: relative;
+               ul {
+                    
+                    display: none;
+                    position: absolute;
+                    left: 100%;
+                    top: -50px;
+                    li {
+                        width: 100px;
+                    }
+               }
+                &:hover ul{
+                display: block;
+                background-color: #fff;
+            }
+        }
         }
 
         h5 {
             margin: 0px 10px 0;
-            font-weight: bold;
+            font-weight:600;
             
         }
     }
